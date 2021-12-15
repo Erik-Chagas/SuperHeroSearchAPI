72ce0e385aeb749ee6ccd656264671aceb001395
Erik <erikxd23@hotmail.com.br>
2021-12-15 20:34:25 -0300

    deploy 1.0v github

--- src/server.ts
+++ src/server.ts
@@ -3,5 +3,5 @@ import { app } from './app'
 const PORT = process.env.PORT || 8080
 
 app.listen(PORT, () => {
-    console.log(`servidor ligado na porta ${PORT} e url localhost:${8080}`)
+    console.log(`servidor ligado`)
 })
