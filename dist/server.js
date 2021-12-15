b3d6fdf7de54544be6c06d78ce9a338e9a062f2d
Erik <erikxd23@hotmail.com.br>
2021-12-15 20:37:37 -0300

    deploy 1.0v github

--- dist/server.js
+++
@@ -1,7 +0,0 @@
-"use strict";var _app = require('./app');
-
-const PORT = process.env.PORT || 8080
-
-_app.app.listen(PORT, () => {
-    console.log(`servidor ligado na porta ${PORT} e url localhost:${8080}`)
-})
